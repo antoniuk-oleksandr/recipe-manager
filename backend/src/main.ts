0af913c18c2a8d28f7e7ddea3c7c723ec8674e74
antoniuk-oleksandr <antonyk063@gmail.com>
2025-09-07 23:03:21 +0300

    feat(backend/dockerfile): add Dockerfile

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT')!;
 
-  await app.listen({ port });
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap();
